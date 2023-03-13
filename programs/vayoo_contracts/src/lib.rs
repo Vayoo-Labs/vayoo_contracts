8124a3eef015998ebd7c1d6e9dcd7baecf78a99f
Adill <adil@mvayoo.markets>
2023-03-14 02:09:06 +0530

    constraint on fns after settle mode is enabed

--- programs/vayoo_contracts/src/lib.rs
+++ programs/vayoo_contracts/src/lib.rs
@@ -78,6 +78,7 @@ pub mod vayoo_contracts {
      * locks 2 * limiting amplitude * nb of tokens for minting - (free -> locked)
      * mints the required contracts
      */
+    #[access_control(unrestricted_trading_phase(&ctx.accounts.contract_state))]
     pub fn mint_l_contract_mm(ctx: Context<MintContractMm>, amount: u64) -> Result<()> {
         mint_lcontract_mm::handle(ctx, amount)
     }
@@ -89,6 +90,7 @@ pub mod vayoo_contracts {
      * unlocks 2 * limiting amplitude * nb of tokens for minting - (locked -> free)
      * burns the required contracts
      */
+    #[access_control(unrestricted_trading_phase(&ctx.accounts.contract_state))]
     pub fn burn_l_contract_mm(ctx: Context<BurnContractMm>, amount: u64) -> Result<()> {
         burn_lcontract_mm::handle(ctx, amount)
     }
@@ -96,6 +98,7 @@ pub mod vayoo_contracts {
     /**
      * Long Contract
      */
+    #[access_control(unrestricted_trading_phase(&ctx.accounts.contract_state))]
     pub fn long_user(
         ctx: Context<LongUser>,
         amount: u64,
@@ -117,6 +120,7 @@ pub mod vayoo_contracts {
     /**
      * Close Long Contract
      */
+    #[access_control(unrestricted_trading_phase(&ctx.accounts.contract_state))]
     pub fn close_long_user(
         ctx: Context<CloseLongUser>,
         amount: u64,
@@ -135,7 +139,52 @@ pub mod vayoo_contracts {
         )
     }
 
+
     /**
+     * Short Contract
+     */
+    #[access_control(unrestricted_trading_phase(&ctx.accounts.contract_state))]
+    pub fn short_user(
+        ctx: Context<ShortUser>,
+        amount: u64,
+        other_amount_threshold: u64,
+        sqrt_price_limit: u128,
+        amount_specified_is_input: bool,
+        a_to_b: bool,
+    ) -> Result<()> {
+        short_user::handle(
+            ctx,
+            amount,
+            other_amount_threshold,
+            sqrt_price_limit,
+            amount_specified_is_input,
+            a_to_b,
+        )
+    }
+
+    /**
+     * Close Short Contract
+     */
+    #[access_control(unrestricted_trading_phase(&ctx.accounts.contract_state))]
+    pub fn close_short_user(
+        ctx: Context<CloseShortUser>,
+        amount: u64,
+        other_amount_threshold: u64,
+        sqrt_price_limit: u128,
+        amount_specified_is_input: bool,
+        a_to_b: bool,
+    ) -> Result<()> {
+        close_short_user::handle(
+            ctx,
+            amount,
+            other_amount_threshold,
+            sqrt_price_limit,
+            amount_specified_is_input,
+            a_to_b,
+        )
+    }
+
+     /**
      * Trigger Settle Mode
      *
      * Can be called by anyone, checks whether current time > maturity time.
@@ -174,47 +223,5 @@ pub mod vayoo_contracts {
      */
     pub fn mm_settle_long(ctx: Context<MmSettleLong>, amount: u64) -> Result<()> {
         mm_settle_long::handle(ctx, amount)
-    }
-
-    /**
-     * Short Contract
-     */
-    pub fn short_user(
-        ctx: Context<ShortUser>,
-        amount: u64,
-        other_amount_threshold: u64,
-        sqrt_price_limit: u128,
-        amount_specified_is_input: bool,
-        a_to_b: bool,
-    ) -> Result<()> {
-        short_user::handle(
-            ctx,
-            amount,
-            other_amount_threshold,
-            sqrt_price_limit,
-            amount_specified_is_input,
-            a_to_b,
-        )
-    }
-
-    /**
-     * Close Short Contract
-     */
-    pub fn close_short_user(
-        ctx: Context<CloseShortUser>,
-        amount: u64,
-        other_amount_threshold: u64,
-        sqrt_price_limit: u128,
-        amount_specified_is_input: bool,
-        a_to_b: bool,
-    ) -> Result<()> {
-        close_short_user::handle(
-            ctx,
-            amount,
-            other_amount_threshold,
-            sqrt_price_limit,
-            amount_specified_is_input,
-            a_to_b,
-        )
     }
 }
