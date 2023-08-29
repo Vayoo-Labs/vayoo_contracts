d0814965556d76518070ec57785c1de975bf5698
Adill <adil@mvayoo.markets>
2023-08-29 11:50:07 +0530

    fix: small comment update

--- tests/utils/constants.ts
+++ tests/utils/constants.ts
@@ -1,6 +1,6 @@
 export const GLOBAL_STATE_SEED = "GLOBAL_STATE_SEED";
 export const PYTH_FEED = "GVXRSBjFk6e6J3NbVPXohDJetcTjaeeuykUpbQF8UoMU"; // (BTC/USD) Pyth Feed on Mainnet
-export const SWITCHBOARD_FEED = "Hemj3drbpJPgUnvrXwqDFp5D49pNBMgQGKvbyD2Uu8g8"; // (BTC/USD) Switchboard Feed on Mainnet
+export const SWITCHBOARD_FEED = "Hemj3drbpJPgUnvrXwqDFp5D49pNBMgQGKvbyD2Uu8g8"; // (RLB/USD) Switchboard Feed on Mainnet
 
 export const ONE_WEEK_IN_SECONDS = 1209600;
 export const USDC_DECIMALS = 6;
